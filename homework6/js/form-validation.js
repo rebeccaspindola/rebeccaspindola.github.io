fb509901684461fa9cfaf54acccee4b446c1f5e8
[Rebecca Alves] <[rebeccaspindola@hotmail.com]>
2020-11-12 19:05:08 -0500

    added comments and folder path for js and css

--- homework6/js/form-validation.js
+++ homework6/js/form-validation.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
+  //validate using jQuery validation function
   $('#input-form').validate({
 
     rules: {
       xFirst: {
-        required: true,
-        number: true
+        required: true, //requires input
+        number: true //verifies input is a number
       },
       xEnd: {
         required: true,
         number: true,
-        greaterThan: '#xFirst'
+        greaterThan: '#xFirst' //verifies that xEnd is greater than xFrist
       },
       yFirst: {
         required: true,
@@ -46,10 +47,12 @@ $(document).ready(function() {
       }
     },
 
+    //add class that turns an invalid form input box red
     highlight: function (input) {
       $(input).removeClass('is-valid').addClass('is-invalid');
     },
 
+    //once error is fixed and input is valid, add a class that turns valid input box green
     unhighlight: function (input) {
       $(input).removeClass('is-invalid').addClass('is-valid');
     }
@@ -62,6 +65,7 @@ $.validator.addMethod( "greaterThan", function (value, element, param) {
     return value > target.val();
 }, "Please enter a greater value.");
 
+//add a custon jQuery validation that returns true in case of greater than input
 $.validator.addMethod( "lessThan", function (value, element, param) {
     var target = $(param);
     return value < target.val();
