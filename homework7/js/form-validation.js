8c1ea25ada61c9891713c9b7e81cab7500541d39
rebeccaspindola <54957134+rebeccaspindola@users.noreply.github.com>
2020-11-25 13:57:20 -0500

    Add files via upload
    Cloning homework 6 to start homework 7

---
+++ homework7/js/form-validation.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+  $('#input-form').validate({
+
+    rules: {
+      xFirst: {
+        required: true,
+        number: true,
+        range: [-100,100]
+      },
+      xEnd: {
+        required: true,
+        number: true,
+        greaterThan: '#xFirst'
+      },
+      yFirst: {
+        required: true,
+        number: true,
+      },
+      yEnd: {
+        required: true,
+        number: true,
+        greaterThan: '#yFirst'
+      }
+    },
+
+    //replace default jQuery error message with custom error messages
+    messages: {
+      xFirst: {
+        required: "Please enter a value between -100 to 100",
+        number: "Error: Input must be a number",
+        //lessThan: "Please enter a value less than the Maximum Column Value"
+      },
+      xEnd: {
+        required: "Please enter a value between -100 to 100",
+        number: "Error: Input must be a number",
+        greaterThan: "Please enter a value greater than the Minimum Column Value"
+      },
+      yFirst: {
+        required: "Please enter a value between -100 to 100",
+        number: "Error: Input must be a number",
+        //lessThan: "Please enter a value less than the Maximum Row Value"
+      },
+      yEnd: {
+        required: "Please enter a value between -100 to 100",
+        number: "Error: Input must be a number",
+        greaterThan: "Please enter a value less than the Minimum Column Value"
+      }
+    },
+
+    highlight: function (input) {
+      $(input).removeClass('is-valid').addClass('is-invalid');
+    },
+
+    unhighlight: function (input) {
+      $(input).removeClass('is-invalid').addClass('is-valid');
+    }
+  });
+});
+
+//Did not end up using
+$.validator.addMethod( "greaterThan", function (value, element, param) {
+    var target = $(param);
+    return value > target.val();
+}, "Please enter a greater value.");
+
+$.validator.addMethod( "lessThan", function (value, element, param) {
+    var target = $(param);
+    return value < target.val();
+}, "Please enter a value less than");
---
+++ homework7/js/script.js
@@ -0,0 +1,79 @@
+function onSubmit() {
+  //assign variables.
+  var minCol = Number(document.getElementById('xFirst').value);
+  var maxCol = Number(document.getElementById('xEnd').value);
+  var minRow = Number(document.getElementById('yFirst').value);
+  var maxRow = Number(document.getElementById('yLast').value);
+
+
+  if(document.getElementById('xFirst').value == '' || document.getElementById('xEnd').value == ''
+    || document.getElementById('yFirst').value == '' || document.getElementById('yLast').value == '') {
+    minCol = 1;
+    maxCol = 10;
+    minRow = 1;
+    maxRow = 10
+  }
+
+  var startX = minCol;
+  var startY = minRow;
+
+  var x;
+  //Create Table
+  var table = document.createElement('table');
+  table.classList.add('newTable');
+
+  //Start table loop. First for loop is for rows. Second is for columns
+  for (let i = 0; i <= getXBounds(minRow,maxRow); i++){
+    const row = document.createElement('tr'); // Create first Row
+
+    startX = minCol; //set X to starting value since we are creating a new column
+
+    for (let j = 0; j <= getYBounds(minCol,maxCol); j++){
+
+      const col = document.createElement('td'); // Create first column
+
+      if ((i == 0) && (j == 0))  { // first square in table is empty
+        val = ''
+        col.classList.add('firstCell');
+
+      } else if(i == 0) {  // First row
+        val = startX;
+        startX++;
+        col.classList.add('firstRow');
+
+      } else if(j == 0) { // New Column
+        val = startY;
+        col.classList.add('firstCol');
+      }
+
+      else { //Multiply
+        val = startX * startY
+        startX++;
+      }
+      col.innerHTML = val;
+      row.appendChild(col);
+
+    }
+
+    if(i != 0) {
+      startY++;
+    }
+
+    table.appendChild(row);
+  }
+
+  document.getElementById('myTable').innerHTML = '';
+  document.getElementById('myTable').appendChild(table);
+
+  return false;
+}
+
+//Next two functions gets the size of i and j in order to build table.
+function getXBounds(xMin, xLast) {
+  return Math.abs(xLast - xMin) + 1;
+}
+
+function getYBounds(yMin, yLast) {
+  return Math.abs(yLast - yMin) + 1;
+}
+x
